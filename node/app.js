06f42db4abfeb6ab2412107e6cf7ed5657f222c8
NahuelValencia <nahuel.valencia21@gmail.com>
2020-05-23 21:33:41 -0300

    more things

--- node/app.js
+++ node/app.js
@@ -14,13 +14,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'TA-TE-TI' });
 });
 
 module.exports = router;
