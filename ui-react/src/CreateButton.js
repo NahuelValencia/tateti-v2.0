e9a35d607ff3dcba60582ba354d26245075f5491
NahuelValencia <nahuel.valencia21@gmail.com>
2020-06-02 02:14:25 -0300

    Create room

--- ui-react/src/CreateButton.js
+++ ui-react/src/CreateButton.js
@@ -1,16 +1,65 @@
 import React from 'react';
+import axios from 'axios';
 
-function CreateButton(props) {
-    console.log("CreateButton")
-    console.log(props)
+class CreateButton extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log("CreateButton")
+        console.log(props)
+        this.state = {
+            room: {},
+            clicked: false
+        }
+    }
 
-    return (
-        <td>
-            <button>
-                Create new game
-            </button>
-        </td>
-    )
+    createRoom = () => {
+        const headers = {
+            'Authorization': this.props.player.session_token
+        }
+
+        const playerId = {
+            playerId: this.props.player.playerId
+        }
+
+        axios
+            .post(`http://localhost:9000/room`, playerId, {
+                headers: headers
+            })
+            .then(res => {
+                if (res.status === 200) {
+                    console.log("Status OK")
+                    console.log(res.data.response)
+                    this.setState({
+                        room: res.data.response,
+                        clicked: true
+                    })
+                }
+            })
+            .catch(error => this.setState({ error, isLoading: false }));
+
+        console.log(this.state)
+    }
+
+
+    render() {
+
+        if (!this.state.clicked) {
+            return (
+                <td>
+                    <button onClick={this.createRoom}>
+                        Create new game
+                </button>
+                </td >
+            )
+        } else {
+            return (
+                <div>
+                    <p>waiting for an oponent</p>
+                    <p>Timer</p>
+                </div>
+            )
+        }
+    }
 }
 
 export default CreateButton;
